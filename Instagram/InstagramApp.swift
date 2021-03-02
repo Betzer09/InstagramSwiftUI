28ddff83bfcdb0f0cd9a4e4dce89e39a8a690937
Betzer09 <betzer.austin09@gmail.com>
2021-03-02 13:24:59 -0700

    Repo

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Austin Betzer on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
