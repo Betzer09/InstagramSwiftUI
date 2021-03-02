cca8acf470b3f587a35e300a4ff758beaebb0f0f
Betzer09 <betzer.austin09@gmail.com>
2021-03-02 13:30:08 -0700

    Added folders stubbs

--- Instagram/Views/Main/ContentView.swift
+++ Instagram/Views/Main/ContentView.swift
@@ -2,15 +2,14 @@
 //  ContentView.swift
 //  Instagram
 //
-//  Created by Austin Betzer on 3/1/21.
+//  Created by Austin Betzer on 3/2/21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -18,4 +17,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-} 
+}
