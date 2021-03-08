079fb3275fc6422e0211164a48a453258db25535
Betzer09 <betzer.austin09@gmail.com>
2021-03-08 13:55:51 -0700

    Added sign up view

--- Instagram/Views/Authentication/RegistrationView.swift
+++ Instagram/Views/Authentication/RegistrationView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct RegistrationView: View {
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var username: String = ""
+    @State private var fullName: String = ""
+    @State private var image: Image?
+    @State private var imagePickerPresented: Bool = false
+    @State private var selectedImage: UIImage?
     @Environment (\.presentationMode) var mode
     
     var body: some View {
@@ -18,9 +23,32 @@ struct RegistrationView: View {
                 .ignoresSafeArea()
             
             VStack {
-                UploadPostView()
-                    .frame(width: 100, height: 100)
-                    .padding([.top, .bottom], 24)
+                
+                if let image = image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 140, height: 140)
+                        .padding(.top, 40)
+                        .clipShape(Circle())
+                } else {
+                    VStack {
+                        Button(action: { imagePickerPresented.toggle() }, label: {
+                            Image(systemName: "plus.circle")
+                                .resizable()
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.black)
+                                .padding(.top, 40)
+                        }).sheet(isPresented: $imagePickerPresented, onDismiss: loadImage, content: {
+                            ImagePicker(image: $selectedImage)
+                        })
+                        
+                        Text("Photo")
+                    }
+                }
+                
+                
+                
                 
                 VStack(spacing: 20) {
                     CustomTextField(text: $email, placeHolder: Text("Email"), imgName: "envelope")
@@ -30,14 +58,14 @@ struct RegistrationView: View {
                         .foregroundColor(.white)
                         .padding(.horizontal, 32)
                     
-                    CustomTextField(text: $email, placeHolder: Text("Username"), imgName: "envelope")
+                    CustomTextField(text: $username, placeHolder: Text("Username"), imgName: "envelope")
                         .padding()
                         .background(Color(.init(white: 1, alpha: 0.15)))
                         .cornerRadius(10)
                         .foregroundColor(.white)
                         .padding(.horizontal, 32)
                     
-                    CustomTextField(text: $email, placeHolder: Text("Full Name"), imgName: "envelope")
+                    CustomTextField(text: $fullName, placeHolder: Text("Full Name"), imgName: "envelope")
                         .padding()
                         .background(Color(.init(white: 1, alpha: 0.15)))
                         .cornerRadius(10)
@@ -77,6 +105,14 @@ struct RegistrationView: View {
         }
     }
 }
+
+extension RegistrationView {
+    func loadImage() {
+        guard let selectedImage = selectedImage else {return}
+        image = Image(uiImage: selectedImage)
+    }
+}
+
 
 struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
